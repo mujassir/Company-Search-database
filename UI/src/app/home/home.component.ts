4b43565fb0beb5ee3869698bc30b1b4f8adff118
Usama Ali <info.usamaali@gmail.com>
2023-11-08 14:39:52 +0500

    Code Refactoring

--- UI/src/app/home/home.component.ts
+++ UI/src/app/home/home.component.ts
@@ -1,29 +1,31 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormControl, FormGroup,FormBuilder } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 import { Table } from 'primeng/table';
 import { CompanyService } from '../services/company.service';
-import { CategoryService } from '../services/category.service';
-import { RegionService } from '../services/region.service';
-import { CountryService } from '../services/country.service';
+import { LovService } from '../services/lov.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  providers: [CountryService]
 })
 export class HomeComponent implements OnInit {
   @ViewChild('companyTable') companyTable!: Table;
   companies!: any;
   companyLoader!: boolean;
+
   categories: any;
+  categoriesLoader!: boolean;
+
   countries: any;
-  regions: any[] = [];
+  countriesLoader!: boolean;
+
+  regions: any;
+  regionsLoader!: boolean;
 
   showFilter: boolean = false;
   searchValue: string = "";
   filterForm!: FormGroup;
-  originalCompanies: any[] = [];
   showFavoriteDialog = false;
   SelectedCompany: any = {};
 
@@ -37,27 +39,22 @@ export class HomeComponent implements OnInit {
   ];
 
   constructor(
-    private categoryService: CategoryService,
     private companyService: CompanyService,
-    private regionService: RegionService,
-    private countryService: CountryService,
+    private lovService: LovService,
     private formBuilder: FormBuilder
   ) {
     this.filterForm = this.formBuilder.group({
       CategoryId: [],
       Country: [],
-      Region: [[]]
+      Region: []
     });
-  }
-  updateRegionSelection(event: any) {
-    this.filterForm.get('Region')?.setValue(event.value);
   }
   ngOnInit(): void {
     this.filterForm = new FormGroup({
       "Country": new FormControl(''),
       "Company": new FormControl(''),
       "Website": new FormControl(''),
-      "CategoryId": new FormControl([]),
+      "CategoryId": new FormControl(''),
       "Region": new FormControl(''),
     });
 
@@ -71,51 +68,32 @@ export class HomeComponent implements OnInit {
     });
 
     this.companyService.companyLoader$.subscribe(loader => this.companyLoader = loader);
+    this.lovService.countriesLoader$.subscribe(loader => this.countriesLoader = loader);
+    this.lovService.regionsLoader$.subscribe(loader => this.regionsLoader = loader);
 
-    this.categoryService.categories$.subscribe(data => {
+    this.lovService.categories$.subscribe(data => {
       // Get Categories if not exist
       if (!data || data.length === 0) {
-        this.getCategories();
+        this.lovService.GetCategories();
       }
       this.categories = data;
     });
 
-    // this.regionService.GetRegions().subscribe((response: any) => {
-    //   this.regions = response.map((region: any) => {
-    //     console.log(region.name)
-    //     return {
-    //       value: region.regionId, 
-    //       label: region.name
-    //     };
-    //   });
-    // });
-    this.regionService.GetRegions().subscribe((response: any) => {
-      this.regions = response.map((region: any) => {
-        return {
-          value: region.name,
-          label: region.name
-        };
-      });
-  
-      // Update the selected region values in the form control
-      const selectedRegions = this.filterForm.get('Region')?.value || [];
-      this.filterForm.get('Region')?.setValue(selectedRegions);
+    this.lovService.countries$.subscribe(data => {
+      // Get Categories if not exist
+      if (!data || data.length === 0) {
+        this.lovService.GetCountries();
+      }
+      this.countries = data;
+    });
+    this.lovService.regions$.subscribe(data => {
+      // Get Categories if not exist
+      if (!data || data.length === 0) {
+        this.lovService.GetRegions();
+      }
+      this.regions = data;
     });
 
-    this.countryService.getCountries().subscribe((data: any) => {
-      this.countries = data.map((country: any) => {
-        console.log(country.name)
-        return {
-          value: country.name,
-          label: country.name
-        };
-      });
-    });
-    
-  }
-
-  getCategories() {
-    this.categoryService.GetCategories();
   }
 
   clickFavoriteBtn(company: any) {
@@ -125,34 +103,8 @@ export class HomeComponent implements OnInit {
 
   // Fetch companies data
   getDataDetails() {
-    const filters = this.filterForm.value;
-  
-    if (!filters.CategoryId && !filters.Country && !filters.Region) {
-      this.companies = this.originalCompanies;
-      return;
-    }
-  
-    this.companies = this.originalCompanies.filter((company: any) => {
-      let matchCategory = true;
-      let matchCountry = true;
-      let matchRegion = true;
-  
-      if (filters.CategoryId && filters.CategoryId.length > 0) {
-        matchCategory = filters.CategoryId.includes(company.categoryId);
-      }
-  
-      if (filters.Country && filters.Country.length > 0) {
-        matchCountry = filters.Country.includes(company.country);
-      }
-  
-      if (filters.Region && filters.Region.length > 0) {
-        matchRegion = filters.Region.some((regionId: number) =>
-          company.regions.some((companyRegion: any) => companyRegion.id === regionId)
-        );
-      }
-  
-      return matchCategory && matchCountry && matchRegion;
-    });
+    this.filterForm.get("Company")?.setValue(this.searchValue)
+    // Call the service to get the filtered data
+    this.companyService.GetCompanies(this.filterForm.value);
   }
 }
-    
---
+++ UI/src/app/services/lov.service.ts
@@ -0,0 +1,79 @@
+import { Injectable } from '@angular/core';
+import { HttpService } from './http.service';
+import { BehaviorSubject, catchError } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LovService {
+  apiUrl = "https://localhost:7047";
+
+  private _categories$ = new BehaviorSubject<any>([]);
+  private _categoryLoader$ = new BehaviorSubject<boolean>(false);
+
+  private _countries$ = new BehaviorSubject<any>([]);
+  private _countriesLoader$ = new BehaviorSubject<boolean>(false);
+
+  private _regions$ = new BehaviorSubject<any>([]);
+  private _regionsLoader$ = new BehaviorSubject<boolean>(false);
+
+  constructor(private http: HttpService) { }
+
+  categories$ = this._categories$.asObservable()
+  categoryLoader$ = this._categoryLoader$.asObservable()
+
+  countries$ = this._countries$.asObservable();
+  countriesLoader$ = this._countriesLoader$.asObservable();
+
+  regions$ = this._regions$.asObservable();
+  regionsLoader$ = this._regionsLoader$.asObservable();
+
+  GetCategories() {
+    this._categoryLoader$.next(true)
+    this.http.SendRequest("get", `${this.apiUrl}/Category`)
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          this._categories$.next([])
+          this._categoryLoader$.next(false);
+          throw error;
+        })
+      )
+      .subscribe(data => {
+        this._categories$.next(data)
+        this._categoryLoader$.next(false);
+      })
+  }
+
+  GetCountries() {
+    this._countriesLoader$.next(true);
+    return this.http.SendRequest("get", `${this.apiUrl}/Country`)
+    .pipe(
+      catchError((error: HttpErrorResponse) => {
+        this._countries$.next([])
+        this._countriesLoader$.next(false);
+        throw error;
+      })
+    )
+    .subscribe(data => {
+      this._countries$.next(data)
+      this._countriesLoader$.next(false);
+    })
+  }
+
+  GetRegions() {
+    this._regionsLoader$.next(true);
+    return this.http.SendRequest("get", `${this.apiUrl}/Region`)
+    .pipe(
+      catchError((error: HttpErrorResponse) => {
+        this._regions$.next([])
+        this._regionsLoader$.next(false);
+        throw error;
+      })
+    )
+    .subscribe(data => {
+      this._regions$.next(data)
+      this._regionsLoader$.next(false);
+    })
+  }
+}
